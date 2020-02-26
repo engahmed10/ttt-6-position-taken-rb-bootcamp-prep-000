fe8ec88356e4f863390c0d5d9cae4d1b7246294f
AHMED AFANDY <eng.computer1@yahoo.com>
2020-02-26 06:17:35 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,11 @@
 def position_taken? (board, index)
    if board[index] == " "
      false
+   elsif board[index] == ""
+     false
+   elsif board[index] == "nil"
+     false
+  elsif board[index] == "X" || board[index] == "O"
+    true
    end
 end
